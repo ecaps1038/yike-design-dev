f089ffe17157678d49d6339bec393832559c42b2
wangyu <wangyu12@accesscorporate.com.cn>
2023-07-03 01:56:39 +0800

    feat(pritter): 格式化

---
+++ src/components/units/upload.vue
@@ -0,0 +1,137 @@
+<script setup lang="ts">
+  import { reactive, ref } from 'vue';
+  import CaseCard from '@/components/CaseCard.vue';
+
+  //说明部分
+  let datas = [
+    {
+      title: '基本用法',
+      note: '单图片上传基本用法。',
+      code: `<Rate />`,
+    },
+  ];
+
+  //api部分
+  const columns = [
+    {
+      title: '参数名',
+      dataIndex: 'name',
+      light: false,
+    },
+    {
+      title: '描述',
+      dataIndex: 'expl',
+      light: false,
+    },
+    {
+      title: '类型',
+      dataIndex: 'type',
+      light: true,
+    },
+    {
+      title: '默认值',
+      dataIndex: 'normal',
+      light: false,
+    },
+  ];
+  const data = reactive([
+    {
+      key: '1',
+      name: 'modelValue(v-model)',
+      expl: '绑定值',
+      type: `number`,
+      normal: 'null',
+    },
+    {
+      key: '2',
+      name: 'disabled',
+      expl: '是否禁用',
+      type: `boolean`,
+      normal: 'false',
+    },
+    {
+      key: '3',
+      name: 'readonly',
+      expl: '是否为只读状态',
+      type: `boolean`,
+      normal: 'false',
+    },
+    {
+      key: '4',
+      name: 'allowHalf',
+      expl: '是否允许半选',
+      type: `boolean`,
+      normal: 'false',
+    },
+    {
+      key: '5',
+      name: 'count',
+      expl: '评分总数',
+      type: `number`,
+      normal: '5',
+    },
+    {
+      key: '6',
+      name: 'color',
+      expl: '颜色',
+      type: `string`,
+      normal: '-',
+    },
+  ]);
+
+  const columns1 = [
+    {
+      title: '事件名',
+      dataIndex: 'name',
+      light: false,
+    },
+    {
+      title: '描述',
+      dataIndex: 'expl',
+      light: false,
+    },
+    {
+      title: '参数',
+      dataIndex: 'type',
+      light: true,
+    },
+  ];
+  const data1 = reactive([
+    {
+      key: '1',
+      name: 'change',
+      expl: '值改变时触发',
+      type: `value: number`,
+    },
+    {
+      key: '2',
+      name: 'hoverChange',
+      expl: '鼠标移动到数值上时触发',
+      type: `value: number`,
+    },
+  ]);
+
+  //复选框内容
+  const yike = ref(3.5);
+</script>
+
+<template>
+  <div class="icons">
+    <div class="top-title">
+      <Title :level="2">Upload 文件上传</Title>
+      <Paragraph>用于常规文件或图片文件的上传。</Paragraph>
+      <CaseCard
+        :title="datas[0].title"
+        :note="datas[0].note"
+        :code="datas[0].code"
+      ></CaseCard>
+    </div>
+  </div>
+</template>
+<style lang="less" scoped>
+  .unit-table {
+    width: 100%;
+    // overflow: hidden;
+    overflow-x: auto;
+  }
+</style>
--- src/yike-design/components/upload/YkUpload.vue
+++ src/yike-design/components/upload/YkUpload.vue
@@ -1,16 +1,62 @@
 <script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+
   const props = defineProps({
-    type: {
+    accept: {
       type: String,
-      default: 'file',
+      default: '*',
     },
-    multiple: {
+    maxSize: {
+      // 最大文件大小，单位字节，0代表不限制
+      type: Number,
+      default: 0,
+    },
+    width: {
+      type: Number,
+      default: 0,
+    },
+    uploadTag: {
+      type: String,
+      default: null,
+    },
+    preview: {
+      // 是否开启预览
+      type: Boolean,
+      default: true,
+    },
+    transcode: {
+      // 是否开启视频转码按钮
       type: Boolean,
       default: false,
     },
+    downloadable: {
+      // 是否可下载
+      type: Boolean,
+      default: false,
+    },
+    equalProportion: {
+      // 是否等比例限制文件宽高
+      type: Boolean,
+      default: false,
+    },
+    uploadUrl: {
+      // 图片上传URL
+      type: String,
+      default: undefined,
+    },
+    size: {
+      type: String,
+      default: 'normal',
+      validator: value => ['small', 'normal', 'large'].includes(value),
+    },
   });
+  const isPicture = ref(false);
+  isPicture.value = props.accept.includes('image');
 </script>
 <template>
-  <div class="yk-upload"></div>
+  <div class="yk-upload-list">
+    <div class="yk-uploader-file">123</div>
+    <div class="yk-uplodaer-picture">123123</div>
+  </div>
 </template>
 <style scoped lang="less"></style>
