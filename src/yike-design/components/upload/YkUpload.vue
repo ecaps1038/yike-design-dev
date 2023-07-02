4b7d56bc5b3546ad8fdeb7a624f7afcfac76b5ed
wangyu <wangyu12@accesscorporate.com.cn>
2023-07-03 01:17:31 +0800

    feat(uploader): 初始化文件上传

---
+++ src/yike-design/components/upload/YkUpload.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  const props = defineProps({
+    type: {
+      type: String,
+      default: 'file',
+    },
+    multiple: {
+      type: Boolean,
+      default: false,
+    },
+  });
+</script>
+<template>
+  <div class="yk-upload"></div>
+</template>
+<style scoped lang="less"></style>
