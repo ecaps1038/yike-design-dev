0c52196f1dbd41695ca4ba895fb84d726dc26496
rickon-DAFEI <wangyu12@accesscorporate.com.cn>
2023-07-04 19:36:46 +0800

    feat(upload): 修改上传组件标签名

--- src/yike-design/components/upload/upload.ts
+++ src/yike-design/components/upload/upload.ts
@@ -8,7 +8,6 @@ export function getArcPath(
   const x = cx + r * Math.sin(endAngle)
   const y = cy - r * Math.cos(endAngle)
 
-  return `M${cx},${cy} L${cx},${cy - r} A${r},${r} 0 ${
-    endAngle < Math.PI ? '0' : '1'
-  },1 ${x},${y} Z`
+  return `M${cx},${cy} L${cx},${cy - r} A${r},${r} 0 ${endAngle < Math.PI ? '0' : '1'
+    },1 ${x},${y} Z`
 }
