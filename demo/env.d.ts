39523515d39b7dcf247366a158b8756ad9c113c2
沈孟平 <artsmp@126.com>
2023-07-11 19:39:22 +0800

    feat: 自动为组件生成类型提示

--- demo/env.d.ts
+++ demo/env.d.ts
@@ -1,11 +1,8 @@
 /// <reference types="vite/client" />
+
+
 declare module '*.md' {
-  import type { ComponentCustomOptions } from "vue"
-  const Component: CompositeOperations
+  import type { Component } from "vue"
+  const Component: Component
   export default Component
-}
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
 }
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,14 +1,27 @@
-import { resolve } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite'
 import createVuePlugin from '@vitejs/plugin-vue';
 import vitePluginMarkdown from './plugins/vite-plugin-md'
+import Components from 'unplugin-vue-components/vite'
 const vuePlugin = createVuePlugin({ include: [/\.vue$/, /\.md$/] }); // 配置可编译 .vue 与 .md 文件
 
+const compDir = fileURLToPath(new URL('../packages/yike-design-ui/src/index.ts', import.meta.url));
+const aliasDir = fileURLToPath(new URL('./src', import.meta.url));
+
+
 export default defineConfig({
-  plugins: [vitePluginMarkdown(), vuePlugin],
+  plugins: [vitePluginMarkdown(), vuePlugin, Components({
+    resolvers: [
+      (componentName) => {
+        if (componentName.startsWith('Yk'))
+          return { name: componentName, from: compDir }
+      },
+    ],
+  })
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'demo/src')
+      '@': aliasDir
     }
   },
   css: {
