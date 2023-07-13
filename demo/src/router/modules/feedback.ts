e12fc70d403176dce98d74c89b182e51f7b25b0f
wengx-unx <1027382710@qq.com>
2023-07-13 18:05:53 +0800

    feat(alert): add route and menu for alert

--- demo/src/router/modules/feedback.ts
+++ demo/src/router/modules/feedback.ts
@@ -1,6 +1,10 @@
 import { RouteRecordRaw } from 'vue-router';
 const feedbackRoutes: Array<RouteRecordRaw> = [
   {
+    path: 'alert',
+    component: () => import('@/examples/alert/doc.md'),
+  },
+  {
     path: 'message',
     component: () => import('@/examples/message/doc.md'),
   },
