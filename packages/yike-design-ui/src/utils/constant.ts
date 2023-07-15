5864c414d6d6573b66fd67971b3fe12316b68224
yike <1334785356@qq.com>
2023-07-15 17:45:57 +0800

    refactor(theme):重构全局样式模块，并修改button组件样式

--- packages/yike-design-ui/src/utils/constant.ts
+++ packages/yike-design-ui/src/utils/constant.ts
@@ -1,17 +1,13 @@
-export const TYPES = [
-  'primary',
-  'secondary',
-  'outline',
-] as const;
+export const TYPES = ['primary', 'secondary', 'outline'] as const
 export type Type = (typeof TYPES)[number]
 
-export const SIZES = ['s', 'm', 'l', 'xl'] as const;
+export const SIZES = ['s', 'm', 'l', 'xl'] as const
 export type Size = (typeof SIZES)[number]
 
-export const SHAPES = ['round', 'circle', 'square'] as const;
+export const SHAPES = ['default', 'round', 'circle', 'square'] as const
 export type Shape = (typeof SHAPES)[number]
 
-export const STATUS = ['success', 'warning', 'danger', 'primary'] as const;
+export const STATUS = ['success', 'warning', 'danger', 'primary'] as const
 export type Status = (typeof STATUS)[number]
 
 export const MESSAGETYPE = ['success', 'info', 'warning', 'error'] as const
