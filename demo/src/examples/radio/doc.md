f69513084b2358c2d7c69d2e3d507eead39ba45e
jason <473341651@qq.com>
2023-07-19 13:44:44 +0800

    feat(radio-group): 补充提交单选框组

--- demo/src/examples/radio/doc.md
+++ demo/src/examples/radio/doc.md
@@ -17,3 +17,15 @@
 基本单选框组使用方式，使用原则，当选项大于 5 个时 推荐使用选项框。使用`yk-radio-group`组件将`yk-radio`组件包裹使用
 <RadioGroupPrimary/>
 :::
+
+:::snippet
+禁用状态单选框组
+设置单选框组的`disabled`属性开启禁用状态
+<RadioGroupDisabled/>
+:::
+
+:::snippet
+禁用状态单选框组
+设置单选框组的`direction`属性设置为`vertical`子元素为垂直方向展示,默认值`horizontal`
+<RadioGroupDirection/>
+:::
--- packages/yike-design-ui/src/components/radio/src/radio-group.ts
+++ packages/yike-design-ui/src/components/radio/src/radio-group.ts
@@ -1,9 +1,11 @@
 import { Size } from '../../../utils/constant';
 import { radioEmits } from './radio';
+export const DIRECTION = ['vertical', 'horizontal'] as const;
 export type RadioGroupProps = {
   modelValue?: number | string | boolean;
   size?: Size;
   disabled?: boolean;
+  direction?: (typeof DIRECTION)[number];
 };
 
 export const radioGroupEmits = radioEmits;
