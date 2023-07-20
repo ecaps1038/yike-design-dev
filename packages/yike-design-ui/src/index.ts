2362f5b34a7414c8027fce14fe413189cc7e1052
du <504455730@qq.com>
2023-07-20 21:54:17 +0800

    fix: 冲突合并

--- packages/yike-design-ui/src/index.ts
+++ packages/yike-design-ui/src/index.ts
@@ -13,6 +13,7 @@ import YkMessage from './components/message';
 import { YkParagraph, YkTitle, YkText } from './components/typography';
 import { YkBackTop } from './components/back-top';
 import YkTooltip from './components/tooltip';
+import YkEmpty from './components/empty';
 import './styles/index.less';
 
 const components: {
@@ -32,6 +33,7 @@ const components: {
   YkParagraph,
   YkTitle,
   YkText,
+  YkEmpty,
   YkBackTop,
   YkTooltip,
 };
@@ -52,6 +54,7 @@ export {
   YkTitle,
   YkText,
   YkMessage,
+  YkEmpty,
   YkBackTop,
   YkTooltip,
 };
