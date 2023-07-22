9ae81f2a43e41e9cb5c0c197b77097ed4022d815
Ghosteye <yqcyecong@163.com>
2023-07-22 14:46:08 +0800

    chore: only allow pnpm

--- config/script/preinstall.js
+++
@@ -1,8 +0,0 @@
-if (!/pnpm/.test(process.env.npm_execpath || '')) {
-  console.log('只能使用pnpm')
-  console.warn(
-    `\u001b[33mThis repository requires using pnpm as the package manager ` +
-      ` for scripts to work properly.\u001b[39m\n`,
-  )
-  process.exit(1)
-}
