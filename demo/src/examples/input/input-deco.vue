fab3b8880067e42b5afd19ad6c340500c749c938
fgt1t5y <1501034130@qq.com>
2023-07-25 07:25:50 +0800

    refactor(input): 样式优化 && 细节修改

--- demo/src/examples/input/input-deco.vue
+++ demo/src/examples/input/input-deco.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="item">
-    <yk-title level="5">搭配文字</yk-title>
     <yk-input placeholder="四">
       <template #prefix>今天是星期</template>
     </yk-input>
   </div>
   <div class="item">
-    <yk-title level="5"></yk-title>
     <yk-input>
       <template #prefix>衬衫的价格是</template>
       <template #suffix>便士</template>
     </yk-input>
   </div>
   <div class="item">
-    <yk-title level="5">搭配图标</yk-title>
     <yk-input placeholder="输入链接">
       <template #prefix>
         <yk-icon name="yk-fujian"></yk-icon>
@@ -21,7 +18,6 @@
     </yk-input>
   </div>
   <div class="item">
-    <yk-title level="5">搭配控件</yk-title>
     <yk-input placeholder="请输入口令...">
       <template #suffix>
         <yk-button>提交</yk-button>
@@ -29,3 +25,11 @@
     </yk-input>
   </div>
 </template>
+<style scoped>
+.item {
+  display: flex;
+  margin-bottom: 5px;
+  width: 350px;
+  flex-direction: column;
+}
+</style>
--- demo/src/examples/input/input-status.vue
+++ demo/src/examples/input/input-status.vue
@@ -1,29 +1,19 @@
 <template>
   <div class="item">
-    <yk-title level="5">不可用</yk-title>
     <yk-input disabled placeholder="不可用" />
   </div>
   <div class="item">
-    <yk-title level="5">危险色</yk-title>
     <yk-input status="danger" placeholder="危险色" />
   </div>
   <div class="item">
-    <yk-title level="5">成功色</yk-title>
     <yk-input status="success" placeholder="成功色" />
   </div>
   <div class="item">
-    <yk-title level="5">警告色</yk-title>
     <yk-input status="warning" placeholder="警告色" />
   </div>
 </template>
 <style scoped>
 .item {
-  display: flex;
-  align-items: center;
   margin-bottom: 5px;
-}
-.item h5 {
-  margin: 0px;
-  width: 65px;
 }
 </style>
