7020df6dbfbb5e73905bb0d8e4a8e58faa6a88e2
rickon-DAFEI <1016831404@qq.com>
2023-07-25 20:18:49 +0800

    feat(upload): change multiple logic

--- packages/yike-design-ui/src/components/upload/src/upload.vue
+++ packages/yike-design-ui/src/components/upload/src/upload.vue
@@ -131,10 +131,7 @@ const currentLength = computed(() => {
   return currentList.value.length
 })
 const uploadDisabled = computed(() => {
-  return !!(
-    (!props.multiple && currentLength.value) ||
-    (props.limit && currentLength.value >= props.limit)
-  )
+  return props.limit && currentLength.value >= props.limit
 })
 
 isPicture.value =
