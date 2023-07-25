1de1357cf69e5d8295420237438f3db447df4c36
printfCYQ <459987870@qq.com>
2023-07-25 10:50:04 +0800

    feat(rate): add size prop

---
+++ demo/src/examples/rate/rate-size.vue
@@ -0,0 +1,10 @@
+<template>
+  <yk-rate size="s"></yk-rate>
+  <br />
+  <yk-rate size="m"></yk-rate>
+  <br />
+  <yk-rate size="l"></yk-rate>
+  <br />
+  <yk-rate size="xl"></yk-rate>
+  <br />
+</template>
