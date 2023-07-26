82a9edbd064bf20c031b7f781b1d11e208bb35af
chiman <1164617068@qq.com>
2023-07-26 20:10:51 +0800

    feat(notification): stop the countdown on hover

--- packages/yike-design-ui/src/components/notification/src/notification.vue
+++ packages/yike-design-ui/src/components/notification/src/notification.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="yk-notification" :style="Style">
+  <div
+    class="yk-notification"
+    :style="Style"
+    @mouseenter="clearTimer"
+    @mouseleave="startTimer"
+  >
     <div class="notification-container">
       <div class="notification-body">
         <div v-if="props.showIcon" class="iconDiv">
@@ -55,6 +60,8 @@ const props = withDefaults(defineProps<NotificationProps>(), {
 
 const emits = defineEmits(['close'])
 
+let timer = 0
+
 const statusIconName = computed(() => {
   return iconStatusMap[props.type]
 })
@@ -63,6 +70,7 @@ const Style = computed(() => ({
   top: `${props.offsetY}px`,
   right: `${props.offsetX}px`,
   zIndex: props.zIndex,
+  marginTop: `0px`,
 }))
 const iconStatusMap = {
   primary: 'yike-tixing',
@@ -71,10 +79,19 @@ const iconStatusMap = {
   success: 'yike-gou',
 }
 function startTimer() {
-  if (!props.duration) return
-  setTimeout(() => {
-    close()
-  }, props.duration)
+  if (props.duration <= 0) return
+  timer = Number(
+    setTimeout(() => {
+      close()
+    }, props.duration),
+  )
+}
+
+function clearTimer() {
+  if (timer) {
+    clearTimeout(timer)
+    timer = 0
+  }
 }
 
 function close() {
