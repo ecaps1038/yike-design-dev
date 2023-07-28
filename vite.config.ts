f4c67445e1b5e01a823bae3a2b6ae2c7a3fc8455
Lin596666 <jin656169299@163.com>
2023-07-28 20:45:30 +0800

    refactor(test): 重构测试子包结构，只保留button测试用例

--- vite.config.ts
+++ vite.config.ts
@@ -38,11 +38,11 @@ export default defineConfig({
       },
     },
   },
-  test: {
-    // enable jest-like global test APIs
-    globals: true,
-    // simulate DOM with happy-dom
-    // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom',
-  },
+  // test: {
+  //   // enable jest-like global test APIs
+  //   globals: true,
+  //   // simulate DOM with happy-dom
+  //   // (requires installing happy-dom as a peer dependency)
+  //   environment: 'happy-dom',
+  // },
 });
