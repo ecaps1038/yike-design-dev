50e8d1e6d6f1e2e3af6b7b9150a2c38dbab6c0fa
fgt1t5y <1501034130@qq.com>
2023-07-28 17:47:43 +0800

    feat(input): 加点注释

--- packages/yike-design-ui/src/components/drawer/src/utils.ts
+++ packages/yike-design-ui/src/components/drawer/src/utils.ts
@@ -10,17 +10,23 @@ let count = 0;
 const DrawerOperator = createGlobalState(() => {
   const drawers = ref<number[]>([]);
 
+  // 记录一个抽屉打开 将抽屉 id 放入 drawers 数组
   const open = (id: number) => {
     drawers.value.push(id);
     console.log('opened drawers', drawers.value);
   };
 
+  // 记录最后一个打开（最前面的）的抽屉关闭
   const close = () => {
     drawers.value.pop();
     console.log('opened drawers', drawers.value);
   };
 
-  return { drawers, open, close };
+  const isLast = (id: number): boolean => {
+    return drawers.value[drawers.value.length - 1] === id ? true : false;
+  };
+
+  return { drawers, open, close, isLast };
 });
 
 export const getDrawerOrder = () => {
