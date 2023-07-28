e4ad56122e8857fad6f59a03f2da14333c3487e3
yike1 <2098933249@qq.com>
2023-07-28 12:17:04 +0800

    fix(rate): 优化rate样式

--- demo/components.d.ts
+++ demo/components.d.ts
@@ -32,6 +32,7 @@ declare module 'vue' {
     YkParagraph: typeof import('./../packages/yike-design-ui/src/index.ts')['YkParagraph']
     YkRadio: typeof import('./../packages/yike-design-ui/src/index.ts')['YkRadio']
     YkRadioGroup: typeof import('./../packages/yike-design-ui/src/index.ts')['YkRadioGroup']
+    YkRate: typeof import('./../packages/yike-design-ui/src/index.ts')['YkRate']
     YkScrollbar: typeof import('./../packages/yike-design-ui/src/index.ts')['YkScrollbar']
     YkSpace: typeof import('./../packages/yike-design-ui/src/index.ts')['YkSpace']
     YkText: typeof import('./../packages/yike-design-ui/src/index.ts')['YkText']
