e5cfcb399237b72c24bdebaf0432c4e14a639855
Yi <504455730@qq.com>
2023-08-01 23:05:47 +0800

    feat(popconfirm): Complete component Popconfirm

--- packages/yike-design-ui/src/components/popconfirm/src/popconfirm.ts
+++ packages/yike-design-ui/src/components/popconfirm/src/popconfirm.ts
@@ -1,3 +1,14 @@
-export type PopconfirmProps = {
-  id?: string;
+import { PopoverProps } from '../../popover/src/popover';
+import { ButtonProps } from '../../button/src/button';
+export interface PopconfirmProps extends PopoverProps {
+  cancelText?: string;
+  okText?: string;
+  showCancel?: boolean;
+  cancelButtonProps?: ButtonProps;
+  okButtonProps?: ButtonProps;
+}
+export type PopconfirmEmit = {
+  (e: 'cancel'): void;
+  (e: 'confirm'): void;
+  (e: 'update:open', ev: boolean): void;
 };
