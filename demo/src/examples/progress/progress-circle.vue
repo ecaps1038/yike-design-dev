ab360b288c87395cccd4a499e1b238e6ab6beae8
Rickon_DAFEI <1016831404@qq.com>
2023-08-03 12:49:14 +0800

    fix(progress): change attrs methods

--- demo/src/examples/progress/progress-circle.vue
+++ demo/src/examples/progress/progress-circle.vue
@@ -1,13 +1,13 @@
 <template>
   <yk-space>
-    <yk-progress :type="'circle'" :size="'s'"></yk-progress>
-    <yk-progress :type="'circle'" :percent="50" :size="'m'"></yk-progress>
-    <yk-progress :type="'circle'" :percent="99" :size="'l'">
+    <yk-progress type="circle" size="s"></yk-progress>
+    <yk-progress type="circle" :percent="50" size="m"></yk-progress>
+    <yk-progress type="circle" :percent="99" size="l">
       <template #format="{ percent }">
         <div style="color: red">剩余{{ percent }}</div>
       </template>
     </yk-progress>
-    <yk-progress :type="'circle'" :percent="num" :size="'l'"></yk-progress>
+    <yk-progress type="circle" :percent="num" size="l"></yk-progress>
   </yk-space>
 </template>
 <script setup lang="ts">
