290a8125363e9502b8dd134f0ab6ffc8ddaf93bc
yi <504455730@qq.com>
2023-08-03 10:08:34 +0800

    docs(tooltip): update tooltip docs

--- demo/src/examples/tooltip/tooltip-open.vue
+++ demo/src/examples/tooltip/tooltip-open.vue
@@ -1,13 +1,19 @@
 <template>
-  <label>
-    <input v-model="open" type="checkbox" />
+  <label class="tooltip-switch">
+    <yk-switch v-model="open"></yk-switch>
     <span>切换气泡显示状态</span>
   </label>
   <yk-tooltip v-model:open="open" placement="right" trigger="none">
-    <yk-button type="outline">tooltip</yk-button>
+    <yk-button>tooltip</yk-button>
   </yk-tooltip>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
 const open = ref(false)
 </script>
+<style scoped>
+.tooltip-switch {
+  display: block;
+  margin-bottom: 8px;
+}
+</style>
