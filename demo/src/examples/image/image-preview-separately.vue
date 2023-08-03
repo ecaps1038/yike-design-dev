f405c97886faecefb093ebf7b50889a6a2aeb3aa
wengx-unx <1027382710@qq.com>
2023-08-04 00:07:58 +0800

    feat(image): add docs for image component

---
+++ demo/src/examples/image/image-preview-separately.vue
@@ -0,0 +1,14 @@
+<template>
+  <yk-button @click="visible = true">
+    <IconEyeOutline />
+    <span>预览</span>
+  </yk-button>
+  <yk-image-preview
+    v-model:visible="visible"
+    src="/src/assets/icon/yike.png"
+  ></yk-image-preview>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const visible = ref(false)
+</script>
