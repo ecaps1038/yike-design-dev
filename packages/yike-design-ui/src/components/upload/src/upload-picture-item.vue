ace928a07745390d5293352ee3e87a9aab37c84d
Rickon_DAFEI <1016831404@qq.com>
2023-08-06 21:05:07 +0800

    fix(upload): fix crop scroll

--- packages/yike-design-ui/src/components/upload/src/upload-picture-item.vue
+++ packages/yike-design-ui/src/components/upload/src/upload-picture-item.vue
@@ -31,14 +31,19 @@
       <IconDeleteOutline @click="handleRemove" />
     </div>
     <yk-modal
-      v-if="editModalVisible"
       v-model="editModalVisible"
       :scrollable="false"
       title="图片裁剪"
       size="small"
       @on-submit="handleSubmit"
     >
-      <cropPicture ref="cropRef" :url="url" :blob-raw="blobRaw" :uid="uid" />
+      <cropPicture
+        v-if="editModalVisible"
+        ref="cropRef"
+        :url="url"
+        :blob-raw="blobRaw"
+        :uid="uid"
+      />
     </yk-modal>
   </div>
 </template>
