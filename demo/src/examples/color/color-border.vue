31f745071ea3718f01302db80883409577bc067f
xuqssq <xuqssq@163.com>
2023-08-29 17:04:00 +0800

    refactor(color): refactor color

---
+++ demo/src/examples/color/color-border.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="ykcolor-background">
+    <ColorCard label="line-color-s" color="#eaeaea" :type="2" class="black" />
+    <ColorCard label="line-color-m" color="#d3d3d3" :type="2" class="black" />
+    <ColorCard label="line-color-l" color="#bdbfc2" :type="2" class="black" />
+  </div>
+</template>
