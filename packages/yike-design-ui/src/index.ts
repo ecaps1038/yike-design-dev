72d89a6b6755656789262632459265ea9e94c36b
liamzi <liamzi6666@163.com>
2023-08-30 10:59:16 +0800

    feat(divider): add divider component

--- packages/yike-design-ui/src/index.ts
+++ packages/yike-design-ui/src/index.ts
@@ -26,6 +26,7 @@ import { YkForm, YkFormItem } from './components/form';
 import YkPagination from './components/pagination';
 import YkTextArea from './components/text-area';
 import YkSkeleton from './components/skeleton';
+import YkDivider from './components/divider';
 import YkAlert from './components/alert';
 import YkButton from './components/button';
 import YkTheme from './components/theme';
@@ -86,6 +87,7 @@ const components: {
   YkFormItem,
   YkTextArea,
   YkSkeleton,
+  YkDivider,
   YkAlert,
   YkButton,
   YkTheme,
@@ -143,6 +145,7 @@ export {
   YkFormItem,
   YkTextArea,
   YkSkeleton,
+  YkDivider,
   YkAlert,
   YkButton,
   YkTheme,
