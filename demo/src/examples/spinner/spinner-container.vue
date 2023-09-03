102f6c5be31f763baff53c66086c946c9e6848f2
望 <1501034130@qq.com>
2023-09-03 17:25:33 +0800

    docs(spinner): 优化demo

--- demo/src/examples/spinner/spinner-container.vue
+++ demo/src/examples/spinner/spinner-container.vue
@@ -1,13 +1,24 @@
 <template>
-  <yk-button @click="loading = !loading">显示/隐藏加载动画</yk-button>
+  <yk-button @click="showLoading">显示加载动画</yk-button>
   <div id="spinner-demo" v-loading="loading" data-loading-text="加载中...">
-    <yk-title :level="3">点击按钮显示加载动画</yk-title>
+    <yk-title :level="4">点击按钮显示加载动画</yk-title>
   </div>
 </template>
 <script setup>
 import { ref } from 'vue'
 
 const loading = ref(false)
+const showLoading = () => {
+  if (loading.value) {
+    return
+  }
+  let timerID = undefined
+  loading.value = true
+  timerID = setTimeout(() => {
+    loading.value = false
+    clearTimeout(timerID)
+  }, 3000)
+}
 </script>
 <style scoped lang="less">
 #spinner-demo {
@@ -18,7 +29,7 @@ const loading = ref(false)
   height: 500px;
   border: 1px solid @gray-1;
   border-radius: 8px;
-  h3 {
+  h4 {
     text-align: center;
   }
 }
