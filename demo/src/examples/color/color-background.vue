da484ab4bcf8986e0254c97604a20e644578e857
yike <1334785356@qq.com>
2023-09-04 23:28:02 +0800

    docs(color): 修改color文档

--- demo/src/examples/color/color-background.vue
+++ demo/src/examples/color/color-background.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="ykcolor-background">
-    <ColorCard label="bg-color-ss" color="#E8E8E8" :type="2" class="black" />
-    <ColorCard label="bg-color-s" color="#F4F4F4" :type="2" class="black" />
-    <ColorCard label="bg-color-m" color="#F7F7F7" :type="2" class="black" />
-    <ColorCard label="bg-color-l" color="#FFFFFF" :type="2" class="white" />
+    <ColorCard label="bg-color-ss" :type="2" class="bg-color-ss" />
+    <ColorCard label="bg-color-s" :type="2" class="bg-color-s" />
+    <ColorCard label="bg-color-m" :type="2" class="bg-color-m" />
+    <ColorCard label="bg-color-l" :type="2" class="bg-color-l bg-l" />
   </div>
 </template>
+<style lang="less" scoped>
+.colors(@name) {
+  .bg-color-@{name} {
+    color: @font-color-l;
+    background-color: ~'@{bg-color-@{name}}';
+  }
+}
+.colors(ss);
+.colors(s);
+.colors(m);
+.colors(l);
+.bg-l {
+  border: 1px solid @line-color-s;
+}
+</style>
