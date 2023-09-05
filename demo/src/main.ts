810869212c3d0542bb56b711ddbf5576c91daee1
Rickon_DAFEI <1016831404@qq.com>
2023-09-05 16:10:34 +0800

    feat(ci): test publish

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -9,7 +9,6 @@ import '@yike-design/ui/src/components/styles/index.less';
 import '@/style/main.less';
 import '@/style/color-card.less';
 
-//highlight 的样式，依赖包，组件
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import hljsVuePlugin from '@highlightjs/vue-plugin';
