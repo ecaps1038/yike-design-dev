e9a5f90c8058811804b4c6e89d4254745b80f291
yike1 <1334785356@qq.com>
2023-09-06 17:32:46 +0800

    fix(anchor): 调整样式

--- demo/src/components/DocPage.vue
+++ demo/src/components/DocPage.vue
@@ -62,8 +62,8 @@ onMounted(() => {
   position: sticky;
   top: 88px;
   float: left;
-  padding: 0 16px 0 56px;
-  width: var(--anchor-width);
+  padding-left: 56px;
+  width: 208px;
 }
 
 /* stylelint-disable-next-line media-feature-range-notation */
