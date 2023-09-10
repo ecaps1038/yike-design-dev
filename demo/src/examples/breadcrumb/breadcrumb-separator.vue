40308640c09f0a7558287825ae3248c1d03f7d83
fumty13 <fumtydt247@163.com>
2023-09-10 15:31:48 +0800

    docs(breadcrumb): remove separatorIcon

--- demo/src/examples/breadcrumb/breadcrumb-separator.vue
+++ demo/src/examples/breadcrumb/breadcrumb-separator.vue
@@ -4,11 +4,11 @@
     <yk-breadcrumb-item>Button</yk-breadcrumb-item>
     <yk-breadcrumb-item>Rate</yk-breadcrumb-item>
   </yk-breadcrumb>
-  <yk-breadcrumb separator-icon="yk-xiexian">
+  <!-- <yk-breadcrumb separator-icon="yk-xiexian">
     <yk-breadcrumb-item>Home</yk-breadcrumb-item>
     <yk-breadcrumb-item>Button</yk-breadcrumb-item>
     <yk-breadcrumb-item>Rate</yk-breadcrumb-item>
-  </yk-breadcrumb>
+  </yk-breadcrumb> -->
   <yk-breadcrumb>
     <template #separator>
       <yk-icon name="yk-xiangyou"></yk-icon>
