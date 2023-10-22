f5f5918c61f6f249b8bc01ee94f3f5187f090552
默小言 <69682691+2113ic@users.noreply.github.com>
2023-10-22 21:43:35 +0800

    fix(alert): use bem (#311)
    * fix(rate): styles when developing the environment
    
    * fix(alert): use bem

--- demo/src/examples/alert/alert-title.vue
+++ demo/src/examples/alert/alert-title.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="yk-alert-group">
-    <yk-alert type="info" title="标题标题" message="这里是 info 提示" />
-    <yk-alert type="success" title="标题标题" message="这里是 success 提示" />
-    <yk-alert type="warning" title="标题标题" message="这里是 warning 提示" />
-    <yk-alert type="error" title="标题标题" message="这里是 error 提示" />
+    <yk-alert type="info" title="标题" message="这里是 info 提示" />
+    <yk-alert type="success" title="标题" message="这里是 success 提示" />
+    <yk-alert type="warning" title="标题" message="这里是 warning 提示" />
+    <yk-alert type="error" title="标题" message="这里是 error 提示" />
   </div>
 </template>
