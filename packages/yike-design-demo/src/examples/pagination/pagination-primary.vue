8239c5d741f4864388c594e0b23597217a170986
2113ic <icxiaoyanz@gmail.com>
2024-03-27 22:29:06 +0800

    docs(pagination): primary demo add slider

--- packages/yike-design-demo/src/examples/pagination/pagination-primary.vue
+++ packages/yike-design-demo/src/examples/pagination/pagination-primary.vue
@@ -1,5 +1,10 @@
 <template>
-  <yk-space dir="vertical" overflow="auto">
-    <yk-pagination fix-width :total="1000" />
-  </yk-space>
+  <yk-slider v-model="value" :min="1" :max="1000" :step="1" />
+  <yk-pagination fix-width :total="value" style="margin-top: 12px" />
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref(30)
+</script>
