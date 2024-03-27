968e1a4a22db269523ee06573d6de7b5dedac20a
2113ic <icxiaoyanz@gmail.com>
2024-03-27 21:37:46 +0800

    refactor(pagination): improve pagination algorithm, add `layouts` option

--- packages/yike-design-ui/components/pagination/src/pagination.ts
+++ packages/yike-design-ui/components/pagination/src/pagination.ts
@@ -1,31 +1,37 @@
 import type { Size } from '../../utils';
-import type { InjectionKey } from 'vue';
+import type { Component, VNode, Ref } from 'vue';
 
-const PAGINATION_CSS_NAMESPACE: InjectionKey<'pagination'> =
-  Symbol('pagination');
-
-interface PaginationProps {
+export interface PaginationProps {
   total: number;
+  current?: number;
+  pageSize?: number;
   defaultCurrent?: number;
   defaultPageSize?: number;
-  disabled?: boolean;
-  pagerCount?: number;
   pageSizeOptions?: number[];
+  pagerCount?: number;
   size?: Size;
+  layouts?: ('pager' | 'prev' | 'next' | 'total' | 'jumper' | 'pageSize')[];
+  prevIcon?: Component;
+  nextIcon?: Component;
   simple?: boolean;
-  showTotal?: boolean;
-  showJumper?: boolean;
-  showPageSize?: boolean;
-  current?: number;
   fixWidth?: boolean;
-  pageSize?: number;
+  disabled?: boolean;
+  hideOnSinglePage?: boolean;
 }
 
-type PaginationEmits = {
-  (e: 'update:current', value: number): void;
+export type PaginationEmits = {
   (e: 'change', current: number): void;
   (e: 'pageSizeChange', pageSize: number): void;
+  (e: 'update:current', current: number): void;
+  (e: 'update:pageSize', pageSize: number): void;
 };
 
-export type { PaginationEmits, PaginationProps };
-export { PAGINATION_CSS_NAMESPACE };
+export type LayoutMapType = Record<string, VNode>;
+
+export type PgnInfo = {
+  _current: Ref<number>;
+  _pageSize: Ref<number>;
+  lastPage: Ref<number>;
+  isFirstPage: Ref<boolean>;
+  isLastPage: Ref<boolean>;
+};
