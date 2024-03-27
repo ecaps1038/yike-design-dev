d1669f1ac434c5d0ff451c8c4e01f85d2a7b6abd
2113ic <icxiaoyanz@gmail.com>
2024-03-27 21:45:54 +0800

    docs(pagination): remove primary demo `layous` prop

--- packages/yike-design-demo/src/examples/pagination/pagination-primary.vue
+++ packages/yike-design-demo/src/examples/pagination/pagination-primary.vue
@@ -1,9 +1,5 @@
 <template>
   <yk-space dir="vertical" overflow="auto">
-    <yk-pagination
-      fix-width
-      :total="1000"
-      :layouts="['pager', 'prev', 'next']"
-    />
+    <yk-pagination fix-width :total="1000" />
   </yk-space>
 </template>
