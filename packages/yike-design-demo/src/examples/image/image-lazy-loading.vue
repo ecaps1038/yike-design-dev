fa22191c8128888e558c8445758479e40039ac70
rickon_DAFEI <1016831404@qq.com>
2024-04-03 01:59:29 +0800

    docs(image): lazy

--- packages/yike-design-demo/src/examples/image/image-lazy-loading.vue
+++ packages/yike-design-demo/src/examples/image/image-lazy-loading.vue
@@ -1,23 +1,15 @@
 <template>
   <yk-space dir="vertical">
-    <!-- <template v-for="item in photosList" :key="item">
+    <yk-scrollbar ref="scrollbar" :height="400">
       <yk-image
+        v-for="(item, index) in photosList"
+        :key="index"
         :src="item"
-        width="400"
         height="400"
         fit="cover"
         :is-lazy="true"
       ></yk-image>
-    </template> -->
-    <yk-image
-      v-for="(item, index) in photosList"
-      :key="index"
-      :src="item"
-      width="400"
-      height="400"
-      fit="cover"
-      :is-lazy="true"
-    ></yk-image>
+    </yk-scrollbar>
     <yk-pagination
       :total="9"
       :page-size="3"
@@ -30,11 +22,9 @@ import { ref } from 'vue'
 
 const scrollbar = ref(null)
 const originList = [
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW1lQD3xIzZbGzGlraKGM0kqbPBXkuCEJKAE4mymM4_fkWuNVKby8dH6oDO_Nt8sZFpec&usqp=CAU',
   'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
   'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
   'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-  'https://photo-static-api.fotomore.com/creative/vcg/veer/612/veer-131952020.jpg?uid=338&timestamp=1711497323&sign=ae1ed9cf05a8d273278ac675b142c110',
   'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
   'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
   'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
@@ -42,7 +32,11 @@ const originList = [
 ]
 const photosList = ref(originList.slice(0, 3))
 
-const handleChange = (e) => {
-  photosList.value = originList.slice(e * 3, (e + 1) * 3)
+const handleChange = async (e) => {
+  scrollbar.value.scrollTo({
+    top: 0,
+  })
+  photosList.value = originList.slice((e - 1) * 3, e * 3)
 }
 </script>
+<style scoped></style>
