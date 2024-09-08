6e05ee0f2b9438a979e22f0faf05a28587c2c0e7
momei <78132554+momei-LJM@users.noreply.github.com>
2024-09-08 10:18:04 +0800

    fix(form): fix validate & add test & up dep (#358)

---
+++ packages/yike-test/form/validate.test.tsx
@@ -0,0 +1,114 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import {
+  FormInstance,
+  YkForm,
+  YkFormItem,
+} from '../../yike-design-ui/components/form/index'
+import { YkInput } from '../../yike-design-ui/components/input'
+import { nextTick, reactive } from 'vue'
+import { YkCheckbox } from '../../yike-design-ui'
+import { RuleType } from '../../yike-design-ui/components/utils/validate/interface'
+describe('Form', () => {
+  it('create', () => {
+    const wrapper = mount(YkForm)
+    expect(wrapper.classes()).toContain('yk-form')
+  })
+
+  it('custom validator', async () => {
+    const form = reactive({
+      phone: '',
+    })
+    const wrapper = mount({
+      setup() {
+        const rules = [
+          {
+            validator: (value, callback) => {
+              const reg =
+                /^1(3\d|4[0-9]|5[0-35-9]|6[567]|7[013-8]|8\d|9[0-35-9])\d{8}$/
+              if (reg.test(value)) {
+                callback()
+              } else {
+                callback('请输入正确的手机号')
+              }
+            },
+            required: true,
+          },
+        ]
+
+        return () => (
+          <YkForm model={form} ref="formRef">
+            <YkFormItem label="手机号" field="phone" rules={rules}>
+              <YkInput v-model={form.phone} placeholder="请输入"></YkInput>
+            </YkFormItem>
+          </YkForm>
+        )
+      },
+    })
+    await nextTick()
+    const formRef = wrapper.findComponent({ ref: 'formRef' }).vm as FormInstance
+    const res = await formRef.validate()
+    expect(res.phone.message).toBe('请输入正确的手机号')
+    form.phone = '17752936113'
+    const res2 = await formRef.validate()
+    expect(res2).toBe(undefined)
+  })
+  it('required', async () => {
+    const form = reactive({
+      check: undefined,
+    })
+    const wrapper = mount({
+      setup() {
+        const rules = [
+          {
+            required: true,
+            message: '请勾选',
+          },
+        ]
+
+        return () => (
+          <YkForm model={form} ref="formRef">
+            <YkFormItem label="check" field="check" rules={rules}>
+              <YkCheckbox v-model:cheked={form.check}></YkCheckbox>
+            </YkFormItem>
+          </YkForm>
+        )
+      },
+    })
+    await nextTick()
+    const formRef = wrapper.findComponent({ ref: 'formRef' }).vm as FormInstance
+    const res = await formRef.validate()
+    expect(res.check.message).toBe('请勾选')
+  })
+  it('boolean', async () => {
+    const form = reactive({
+      check: false,
+    })
+    const wrapper = mount({
+      setup() {
+        const rules = [
+          {
+            type: 'boolean' as RuleType,
+            true: true,
+            message: '请勾选',
+          },
+        ]
+
+        return () => (
+          <YkForm model={form} ref="formRef">
+            <YkFormItem label="手机号" field="check" rules={rules} required>
+              <YkCheckbox v-model:cheked={form.check}></YkCheckbox>
+            </YkFormItem>
+          </YkForm>
+        )
+      },
+    })
+    await nextTick()
+    const formRef = wrapper.findComponent({ ref: 'formRef' }).vm as FormInstance
+    const res = await formRef.validate()
+    expect(res.check.message).toBe('请勾选')
+    form.check = true
+    const res2 = await formRef.validate()
+    expect(res2).toBe(undefined)
+  })
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vitest/config';
-
+import Vue from '@vitejs/plugin-vue';
+import VueJsx from '@vitejs/plugin-vue-jsx';
 export default defineConfig({
   test: {
+    testTransformMode: {
+      web: ['*.{ts,tsx}'],
+    },
     // enable jest-like global test APIs
     globals: true,
     // simulate DOM with happy-dom
     // (requires installing happy-dom as a peer dependency)
     environment: 'happy-dom',
   },
+  plugins: [Vue(), VueJsx()],
 });
